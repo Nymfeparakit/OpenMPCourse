e6ac0195dcc97ee87b99fa2a8bbf3f79ac51b99f
AleksandraYakupova <maxlex047@gmail.com>
2019-10-05 10:44:02 +0300

    Изменен main

--- Otik/Otik_Lab2_1/Otik_Lab2_1/Source.cpp
+++ Otik/Otik_Lab2_1/Otik_Lab2_1/Source.cpp
@@ -12,13 +12,8 @@
 
 void countSymbolsFrequencies(std::string fileName, std::map <wchar_t, int> &symbolsFrequencies) {
 
-	
-	//std::locale utf8_locale(std::locale(), new gel::stdx::utf8cvt<true>);
 	std::wifstream file(fileName);
 	file.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t, 0x10ffff, std::consume_header>));
-	//fs.imbue(utf8_locale);
-	//file.imbue(std::locale("zh_CN.UTF-8"));
-	//std::map <char, int> symbolsFrequencies;
 	std::wstring line;
 	while (std::getline(file, line)) { //читаем файл построчно
 	
@@ -34,18 +29,16 @@ void countSymbolsFrequencies(std::string fileName, std::map <wchar_t, int> &symb
 			else { //иначе вставл€ем новый элемент
 				symbolsFrequencies.insert({ c, 1 });
 			}
-			//if (iss.eof()) break;
 		}
 	}
 }
 
-//void writeFrequencies(std::map <wchar_t, int> symbolsFrequencies, int count) {
 void writeFrequencies(std::map <wchar_t, int> symbolsFrequencies, std::string fileName) {
 
 	std::wofstream fs;
 	fs.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t, 0x10ffff, std::generate_header>));
 	//size_t dot_pos = fileName.find(".");
-	std::string newFileName = "freq " + fileName.substr(0, fileName.find(".")) + "2.txt";
+	std::string newFileName = "freq " + fileName.substr(0, fileName.find(".")) + ".txt";
 	//std::string newFileName = "freq" + std::to_string(count) + ".txt";
 	fs.open(newFileName, std::fstream::out | std::fstream::trunc);
 	std::map<wchar_t, int>::iterator it;
@@ -86,21 +79,6 @@ void traverseToCreateCodes(Node* node, std::map <wchar_t, std::string> &symbolsC
 
 void createSymbolsCodes(std::map <wchar_t, int> symbolsFrequencies, 
 	std::map <wchar_t, std::string> &symbolsCodes) {
-	
-	//сортируем частоты по возрастанию
-	//объ€вл€ем новый компаратор
-	/*typedef std::function<bool(std::pair<wchar_t, int>, std::pair<wchar_t, int>)> Comparator;
-	Comparator compFunctor =
-		[](std::pair<wchar_t, int> e1, std::pair<wchar_t, int> e2) {
-		return e1.second > e2.second;
-	};
-	//в setOfSymbolsFreqs все символы будут осортированы по возрастанию частоты
-	std::set<std::pair<wchar_t, int>, Comparator> setOfSymbolsFreqs(
-		symbolsFrequencies.begin(), symbolsFrequencies.end(), compFunctor);
-
-	for (std::pair<wchar_t, int> element : setOfSymbolsFreqs) {
-
-	}*/
 
 	std::vector<Node*> orphans;
 	typedef std::function<bool(Node* node1, Node* node2)> Comparator;
@@ -157,21 +135,22 @@ int main() {
 	
 	setlocale(LC_ALL, "Russian");
 	std::string fileName;
-	int counter = 0;
 	for (;;) {
+
+		std::map <wchar_t, int> symbolsFrequencies;
+		std::map <wchar_t, std::string> symbolsCodes;
 		std::cout << "File: ";
 		std::getline(std::cin, fileName);
-		++counter;
-		std::map <wchar_t, int> symbolsFrequencies;
-		countSymbolsFrequencies("test.txt", symbolsFrequencies);
-		std::map <wchar_t, std::string> symbolsCodes;
-		createSymbolsCodes(symbolsFrequencies, symbolsCodes);
-		encodeMessage("test.txt", symbolsCodes);
-		//writeFrequencies(symbolsFrequencies, "Lermontov.txt");
-		if (counter >= 5) {
-			counter = 0;
+		if (fileName.compare("0") == 0) {
+			countSymbolsFrequencies("test.txt", symbolsFrequencies);
+			createSymbolsCodes(symbolsFrequencies, symbolsCodes);
+			encodeMessage("test.txt", symbolsCodes);
 		}
-		//std::cout << std::endl;
+		else {
+			countSymbolsFrequencies(fileName, symbolsFrequencies);
+			writeFrequencies(symbolsFrequencies, fileName);
+		}
+
 	}
 		//system("pause");
 	return 0;
