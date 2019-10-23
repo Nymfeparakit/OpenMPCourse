9e5154f84081d8426349957371690756fdf5122f
AleksandraYakupova <maxlex047@gmail.com>
2019-10-23 14:15:28 +0300

    Добавлены тесты

--- Testing/Testing_Lab3/test_calcui.cpp
+++ Testing/Testing_Lab3/test_calcui.cpp
@@ -1,95 +1,494 @@
 #include "test_calcui.h"
-#include "calcpresenter.h"
+#include <QTimer>
 
 Test_CalcUI::Test_CalcUI()
 {
+}
 
+void Test_CalcUI::initTestCase()
+{
+    calcView = new CalcView();
+    calcView->show();
+    calc = new Calc;
+    calcPresenter = new CalcPresenter(calcView, calc);
+}
+
+void Test_CalcUI::cleanup()
+{
+    calcView->LEditFirstArg->clear();
+    calcView->lEditSecondArg->clear();
+}
+
+Test_CalcUI::~Test_CalcUI()
+{
+    delete calcView;
+    delete calc;
+    delete calcPresenter;
 }
 
 void Test_CalcUI::plusClicked_numberInResultFieldIsCorrect()
 {
     //Arrange
-    CalcView calcView;
+    /*CalcView calcView;
     calcView.show();
     Calc calc;
-    CalcPresenter calcPres(&calcView, &calc);
+    CalcPresenter calcPres(&calcView, &calc);*/
 
     //Act
-    QTest::keyClicks(calcView.LEditFirstArg, "12");
-    QTest::keyClicks(calcView.lEditSecondArg, "2");
-    QTest::mouseClick(calcView.pushBtnPlus, Qt::LeftButton);
+    QTest::keyClicks(calcView->LEditFirstArg, "12");
+    QTest::keyClicks(calcView->lEditSecondArg, "2");
+    QTest::mouseClick(calcView->pushBtnPlus, Qt::LeftButton);
 
     //Assert
-    QCOMPARE(calcView.lEditRes->text(), "14");
+    QCOMPARE(calcView->lEditRes->text(), "14");
 }
 
 void Test_CalcUI::minusClicked_numberInResultFieldIsCorrect()
 {
     //Arrange
-    CalcView calcView;
+    /*CalcView calcView;
     calcView.show();
     Calc calc;
-    CalcPresenter calcPres(&calcView, &calc);
+    CalcPresenter calcPres(&calcView, &calc);*/
 
     //Act
-    QTest::keyClicks(calcView.LEditFirstArg, "12");
-    QTest::keyClicks(calcView.lEditSecondArg, "2");
-    QTest::mouseClick(calcView.pushBtnMinus, Qt::LeftButton);
+    QTest::keyClicks(calcView->LEditFirstArg, "12");
+    QTest::keyClicks(calcView->lEditSecondArg, "2");
+    QTest::mouseClick(calcView->pushBtnMinus, Qt::LeftButton);
 
     //Assert
-    QCOMPARE(calcView.lEditRes->text(), "10");
+    QCOMPARE(calcView->lEditRes->text(), "10");
 }
 
 void Test_CalcUI::multiplyClicked_numberInResultFieldIsCorrect()
 {
     //Arrange
-    CalcView calcView;
+    /*CalcView calcView;
     calcView.show();
     Calc calc;
-    CalcPresenter calcPres(&calcView, &calc);
+    CalcPresenter calcPres(&calcView, &calc);*/
 
     //Act
-    QTest::keyClicks(calcView.LEditFirstArg, "12");
-    QTest::keyClicks(calcView.lEditSecondArg, "2");
-    QTest::mouseClick(calcView.pushBtnMultiply, Qt::LeftButton);
+    QTest::keyClicks(calcView->LEditFirstArg, "12");
+    QTest::keyClicks(calcView->lEditSecondArg, "2");
+    QTest::mouseClick(calcView->pushBtnMultiply, Qt::LeftButton);
 
     //Assert
-    QCOMPARE(calcView.lEditRes->text(), "24");
+    QCOMPARE(calcView->lEditRes->text(), "24");
 }
 
 void Test_CalcUI::divideClicked_numberInResultFieldIsCorrect()
 {
     //Arrange
-    CalcView calcView;
+    /*CalcView calcView;
     calcView.show();
     Calc calc;
-    CalcPresenter calcPres(&calcView, &calc);
+    CalcPresenter calcPres(&calcView, &calc);*/
 
     //Act
-    QTest::keyClicks(calcView.LEditFirstArg, "12");
-    QTest::keyClicks(calcView.lEditSecondArg, "2");
-    QTest::mouseClick(calcView.pushBtnDivide, Qt::LeftButton);
+    QTest::keyClicks(calcView->LEditFirstArg, "12");
+    QTest::keyClicks(calcView->lEditSecondArg, "2");
+    QTest::mouseClick(calcView->pushBtnDivide, Qt::LeftButton);
 
     //Assert
-    QCOMPARE(calcView.lEditRes->text(), "6");
+    QCOMPARE(calcView->lEditRes->text(), "6");
 }
 void Test_CalcUI::divideClickedSecondArgIsZero_msgBoxWithErrorShows()
 {
     //Arrange
-    CalcView calcView;
+    /*CalcView calcView;
     calcView.show();
     Calc calc;
-    CalcPresenter calcPres(&calcView, &calc);
+    CalcPresenter calcPres(&calcView, &calc);*/
+    //calcView.setSingleShotMsgError(true);
 
     //Act
-    QTest::keyClicks(calcView.LEditFirstArg, "12");
-    QTest::keyClicks(calcView.lEditSecondArg, "0");
-    QTest::mouseClick(calcView.pushBtnDivide, Qt::LeftButton);
+    QTest::keyClicks(calcView->LEditFirstArg, "12");
+    QTest::keyClicks(calcView->lEditSecondArg, "0");
+    QTimer::singleShot(0, calcView->msgBxError, SLOT(accept()));
+    QTest::mouseClick(calcView->pushBtnDivide, Qt::LeftButton);
+    //QTimer::singleShot(200, this, SLOT(updateCaption()));
 
     //Assert
-    QVERIFY2(calcView.msgBxError, "Message box not created");
-    QVERIFY2(calcView.msgBxError->isVisible(), "Message box is not visible");
-    QCOMPARE(calcView.msgBxError->text(), "Деление на 0");
+    QCOMPARE(calcView->msgBxError->text(), "Деление на 0");
 
-    QTest::mouseClick(calcView.msgBxError->)
 }
+
+void Test_CalcUI::inputFirstArgIsNotANumber()
+{
+    QTest::keyClicks(calcView->LEditFirstArg, "not a number");
+    QTest::keyClicks(calcView->lEditSecondArg, "2");
+    QTimer::singleShot(200, calcView->msgBxError, SLOT(accept()));
+}
+
+void Test_CalcUI::plusClickedFirstArgIsNotNumber_msgBoxWithErrorShows()
+{
+    //Act
+    inputFirstArgIsNotANumber();
+    QTest::mouseClick(calcView->pushBtnPlus, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+
+void Test_CalcUI::minusClickedFirstArgIsNotNumber_msgBoxWithErrorShows()
+{
+    inputFirstArgIsNotANumber();
+    QTest::mouseClick(calcView->pushBtnMinus, Qt::LeftButton);
+    //QTimer::singleShot(200, this, SLOT(updateCaption()));
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::multiplyClickedFirstArgIsNotNumber_msgBoxWithErrorShows()
+{
+    inputFirstArgIsNotANumber();
+    QTest::mouseClick(calcView->pushBtnMultiply, Qt::LeftButton);
+    //QTimer::singleShot(200, this, SLOT(updateCaption()));
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::divideClickedFirstArgIsNotNumber_msgBoxWithErrorShows()
+{
+    inputFirstArgIsNotANumber();
+    QTest::mouseClick(calcView->pushBtnDivide, Qt::LeftButton);
+    //QTimer::singleShot(200, this, SLOT(updateCaption()));
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::inputSecondArgIsNotANumber()
+{
+    QTest::keyClicks(calcView->LEditFirstArg, "3");
+    QTest::keyClicks(calcView->lEditSecondArg, "not a number");
+    QTimer::singleShot(200, calcView->msgBxError, SLOT(accept()));
+}
+
+void Test_CalcUI::plusClickedSecondArgIsNotNumber_msgBoxWithErrorShows()
+{
+    //Act
+    inputSecondArgIsNotANumber();
+    QTest::mouseClick(calcView->pushBtnPlus, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+
+void Test_CalcUI::minusClickedSecondArgIsNotNumber_msgBoxWithErrorShows()
+{
+    inputSecondArgIsNotANumber();
+    QTest::mouseClick(calcView->pushBtnMinus, Qt::LeftButton);
+    //QTimer::singleShot(200, this, SLOT(updateCaption()));
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::multiplyClickedSecondArgIsNotNumber_msgBoxWithErrorShows()
+{
+    inputSecondArgIsNotANumber();
+    QTest::mouseClick(calcView->pushBtnMultiply, Qt::LeftButton);
+    //QTimer::singleShot(200, this, SLOT(updateCaption()));
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::divideClickedSecondArgIsNotNumber_msgBoxWithErrorShows()
+{
+    inputSecondArgIsNotANumber();
+    QTest::mouseClick(calcView->pushBtnDivide, Qt::LeftButton);
+    //QTimer::singleShot(200, this, SLOT(updateCaption()));
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::inputFirstArgIsOutOfRange()
+{
+    QTest::keyClicks(calcView->LEditFirstArg, "2e+308");
+    QTest::keyClicks(calcView->lEditSecondArg, "2");
+    QTimer::singleShot(0, calcView->msgBxError, SLOT(accept()));
+}
+
+void Test_CalcUI::plusClickedFirstArgIsOutOfRange_msgBoxWithErrorShows()
+{
+    //Act
+    inputFirstArgIsOutOfRange();
+    QTest::mouseClick(calcView->pushBtnPlus, Qt::LeftButton);
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "Аргументы не должны выходить "
+                                           "за пределы допустимых значений");
+}
+
+void Test_CalcUI::minusClickedFirstArgIsOutOfRange_msgBoxWithErrorShows()
+{
+    //Act
+    inputFirstArgIsOutOfRange();
+    QTest::mouseClick(calcView->pushBtnMinus, Qt::LeftButton);
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "Аргументы не должны выходить "
+                                           "за пределы допустимых значений");
+}
+
+void Test_CalcUI::multiplyClickedFirstArgIsOutOfRange_msgBoxWithErrorShows()
+{
+    //Act
+    inputFirstArgIsOutOfRange();
+    QTest::mouseClick(calcView->pushBtnMultiply, Qt::LeftButton);
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "Аргументы не должны выходить "
+                                           "за пределы допустимых значений");
+}
+
+void Test_CalcUI::divideClickedFirstArgIsOutOfRange_msgBoxWithErrorShows()
+{
+    //Act
+    inputFirstArgIsOutOfRange();
+    QTest::mouseClick(calcView->pushBtnDivide, Qt::LeftButton);
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "Аргументы не должны выходить "
+                                           "за пределы допустимых значений");
+}
+
+void Test_CalcUI::inputSecondArgIsOutOfRange()
+{
+    QTest::keyClicks(calcView->LEditFirstArg, "3");
+    QTest::keyClicks(calcView->lEditSecondArg, "2e+308");
+    QTimer::singleShot(0, calcView->msgBxError, SLOT(accept()));
+}
+
+void Test_CalcUI::plusClickedSecondArgIsOutOfRange_msgBoxWithErrorShows()
+{
+    //Act
+    inputSecondArgIsOutOfRange();
+    QTest::mouseClick(calcView->pushBtnPlus, Qt::LeftButton);
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "Аргументы не должны выходить "
+                                           "за пределы допустимых значений");
+}
+
+void Test_CalcUI::minusClickedSecondArgIsOutOfRange_msgBoxWithErrorShows()
+{
+    //Act
+    inputSecondArgIsOutOfRange();
+    QTest::mouseClick(calcView->pushBtnMinus, Qt::LeftButton);
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "Аргументы не должны выходить "
+                                           "за пределы допустимых значений");
+}
+
+void Test_CalcUI::multiplyClickedSecondArgIsOutOfRange_msgBoxWithErrorShows()
+{
+    //Act
+    inputSecondArgIsOutOfRange();
+    QTest::mouseClick(calcView->pushBtnMultiply, Qt::LeftButton);
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "Аргументы не должны выходить "
+                                           "за пределы допустимых значений");
+}
+
+void Test_CalcUI::divideClickedSecondArgIsOutOfRange_msgBoxWithErrorShows()
+{
+    //Act
+    inputSecondArgIsOutOfRange();
+    QTest::mouseClick(calcView->pushBtnDivide, Qt::LeftButton);
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "Аргументы не должны выходить "
+                                           "за пределы допустимых значений");
+}
+
+void  Test_CalcUI::inputFirstArgIsEmpty()
+{
+    QTest::keyClicks(calcView->LEditFirstArg, "");
+    QTest::keyClicks(calcView->lEditSecondArg, "2");
+    QTimer::singleShot(0, calcView->msgBxError, SLOT(accept()));
+}
+
+void Test_CalcUI::plusClickedFirstArgIsEmpty_setErrorMsg()
+{
+    //Act
+    inputFirstArgIsEmpty();
+    QTest::mouseClick(calcView->pushBtnPlus, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::minusClickedFirstArgIsEmpty_setErrorMsg()
+{
+    //Act
+    inputFirstArgIsEmpty();
+    QTest::mouseClick(calcView->pushBtnMinus, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::multiplyClickedFirstArgIsEmpty_setErrorMsg()
+{
+    //Act
+    inputFirstArgIsEmpty();
+    QTest::mouseClick(calcView->pushBtnMultiply, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::divideClickedFirstArgIsEmpty_setErrorMsg()
+{
+    //Act
+    inputFirstArgIsEmpty();
+    QTest::mouseClick(calcView->pushBtnDivide, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void  Test_CalcUI::inputSecondArgIsEmpty()
+{
+    QTest::keyClicks(calcView->LEditFirstArg, "3");
+    QTest::keyClicks(calcView->lEditSecondArg, "");
+    QTimer::singleShot(0, calcView->msgBxError, SLOT(accept()));
+}
+
+void Test_CalcUI::plusClickedSecondArgIsEmpty_setErrorMsg()
+{
+    //Act
+    inputSecondArgIsEmpty();
+    QTest::mouseClick(calcView->pushBtnPlus, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::minusClickedSecondArgIsEmpty_setErrorMsg()
+{
+    //Act
+    inputSecondArgIsEmpty();
+    QTest::mouseClick(calcView->pushBtnMinus, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::multiplyClickedSecondArgIsEmpty_setErrorMsg()
+{
+    //Act
+    inputSecondArgIsEmpty();
+    QTest::mouseClick(calcView->pushBtnMultiply, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::divideClickedSecondArgIsEmpty_setErrorMsg()
+{
+    //Act
+    inputSecondArgIsEmpty();
+    QTest::mouseClick(calcView->pushBtnDivide, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::inputFirstArgWithCommaAsSeparator()
+{
+    QTest::keyClicks(calcView->LEditFirstArg, "3,2");
+    QTest::keyClicks(calcView->lEditSecondArg, "44");
+    QTimer::singleShot(0, calcView->msgBxError, SLOT(accept()));
+}
+
+void Test_CalcUI::plusClickedFirstArgUsesCommaAsSeparator_setErrorMsg()
+{
+    //Act
+    inputFirstArgWithCommaAsSeparator();
+    QTest::mouseClick(calcView->pushBtnPlus, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::minusClickedFirstArgUsesCommaAsSeparator_setErrorMsg()
+{
+    //Act
+    inputFirstArgWithCommaAsSeparator();
+    QTest::mouseClick(calcView->pushBtnMinus, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::multiplyClickedFirstArgUsesCommaAsSeparator_setErrorMsg()
+{
+    //Act
+    inputFirstArgWithCommaAsSeparator();
+    QTest::mouseClick(calcView->pushBtnMultiply, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::divideClickedFirstArgUsesCommaAsSeparator_setErrorMsg()
+{
+    //Act
+    inputFirstArgWithCommaAsSeparator();
+    QTest::mouseClick(calcView->pushBtnDivide, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::inputSecondArgWithCommaAsSeparator()
+{
+    QTest::keyClicks(calcView->LEditFirstArg, "42");
+    QTest::keyClicks(calcView->lEditSecondArg, "4,2");
+    QTimer::singleShot(0, calcView->msgBxError, SLOT(accept()));
+}
+
+void Test_CalcUI::plusClickedSecondArgUsesCommaAsSeparator_setErrorMsg()
+{
+    //Act
+    inputSecondArgWithCommaAsSeparator();
+    QTest::mouseClick(calcView->pushBtnPlus, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::minusClickedSecondArgUsesCommaAsSeparator_setErrorMsg()
+{
+    //Act
+    inputSecondArgWithCommaAsSeparator();
+    QTest::mouseClick(calcView->pushBtnMinus, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::multiplyClickedSecondArgUsesCommaAsSeparator_setErrorMsg()
+{
+    //Act
+    inputSecondArgWithCommaAsSeparator();
+    QTest::mouseClick(calcView->pushBtnMultiply, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
+void Test_CalcUI::divideClickedSecondArgUsesCommaAsSeparator_setErrorMsg()
+{
+    //Act
+    inputSecondArgWithCommaAsSeparator();
+    QTest::mouseClick(calcView->pushBtnDivide, Qt::LeftButton);
+
+    //Assert
+    QCOMPARE(calcView->msgBxError->text(), "В качестве аргумента должно быть число");
+}
+
--- Testing/Testing_Lab3/test_calcui.h
+++ Testing/Testing_Lab3/test_calcui.h
@@ -1,18 +1,77 @@
 #ifndef TEST_CALCUI_H
 #define TEST_CALCUI_H
 #include <QTest>
+#include "calcpresenter.h"
 
 class Test_CalcUI : public QObject
 {
     Q_OBJECT
 public:
     Test_CalcUI();
+    ~Test_CalcUI();
 private slots:
+    //void init();
+    void initTestCase();
+    void cleanup();
+
     void plusClicked_numberInResultFieldIsCorrect();
     void minusClicked_numberInResultFieldIsCorrect();
     void multiplyClicked_numberInResultFieldIsCorrect();
     void divideClicked_numberInResultFieldIsCorrect();
+
     void divideClickedSecondArgIsZero_msgBoxWithErrorShows();
+
+    void plusClickedFirstArgIsNotNumber_msgBoxWithErrorShows();
+    void minusClickedFirstArgIsNotNumber_msgBoxWithErrorShows();
+    void multiplyClickedFirstArgIsNotNumber_msgBoxWithErrorShows();
+    void divideClickedFirstArgIsNotNumber_msgBoxWithErrorShows();
+
+    void plusClickedSecondArgIsNotNumber_msgBoxWithErrorShows();
+    void minusClickedSecondArgIsNotNumber_msgBoxWithErrorShows();
+    void multiplyClickedSecondArgIsNotNumber_msgBoxWithErrorShows();
+    void divideClickedSecondArgIsNotNumber_msgBoxWithErrorShows();
+
+    void plusClickedFirstArgIsOutOfRange_msgBoxWithErrorShows();
+    void minusClickedFirstArgIsOutOfRange_msgBoxWithErrorShows();
+    void multiplyClickedFirstArgIsOutOfRange_msgBoxWithErrorShows();
+    void divideClickedFirstArgIsOutOfRange_msgBoxWithErrorShows();
+
+    void plusClickedSecondArgIsOutOfRange_msgBoxWithErrorShows();
+    void minusClickedSecondArgIsOutOfRange_msgBoxWithErrorShows();
+    void multiplyClickedSecondArgIsOutOfRange_msgBoxWithErrorShows();
+    void divideClickedSecondArgIsOutOfRange_msgBoxWithErrorShows();
+
+    void plusClickedFirstArgIsEmpty_setErrorMsg();
+    void minusClickedFirstArgIsEmpty_setErrorMsg();
+    void multiplyClickedFirstArgIsEmpty_setErrorMsg();
+    void divideClickedFirstArgIsEmpty_setErrorMsg();
+
+    void plusClickedSecondArgIsEmpty_setErrorMsg();
+    void minusClickedSecondArgIsEmpty_setErrorMsg();
+    void multiplyClickedSecondArgIsEmpty_setErrorMsg();
+    void divideClickedSecondArgIsEmpty_setErrorMsg();
+
+    void plusClickedFirstArgUsesCommaAsSeparator_setErrorMsg();
+    void minusClickedFirstArgUsesCommaAsSeparator_setErrorMsg();
+    void multiplyClickedFirstArgUsesCommaAsSeparator_setErrorMsg();
+    void divideClickedFirstArgUsesCommaAsSeparator_setErrorMsg();
+
+    void plusClickedSecondArgUsesCommaAsSeparator_setErrorMsg();
+    void minusClickedSecondArgUsesCommaAsSeparator_setErrorMsg();
+    void multiplyClickedSecondArgUsesCommaAsSeparator_setErrorMsg();
+    void divideClickedSecondArgUsesCommaAsSeparator_setErrorMsg();
+private:
+    void inputFirstArgIsNotANumber();
+    void inputSecondArgIsNotANumber();
+    void inputFirstArgIsOutOfRange();
+    void inputSecondArgIsOutOfRange();
+    void inputFirstArgIsEmpty();
+    void inputSecondArgIsEmpty();
+    void inputFirstArgWithCommaAsSeparator();
+    void inputSecondArgWithCommaAsSeparator();
+    CalcView* calcView;
+    Calc* calc;
+    CalcPresenter* calcPresenter;
 };
 
 #endif // TEST_CALCUI_H
