e3c560c82da27c55cfca5a145dc0ad01e572cae0
theFeline <maxlex047@gmail.com>
2019-12-16 07:39:40 +0300

    Исключение последнего процесса

--- MPI/MPI_Lab6/MPI_Lab6/Source.cpp
+++ MPI/MPI_Lab6/MPI_Lab6/Source.cpp
@@ -41,10 +41,20 @@ char **allocate_2d_array(int rows, int columns) {
 
 }
 
+long long to_dec(longint a) {
+
+	long long res = 0;
+	for (int i = 0; i < a.number.size(); ++i) {
+		res += a.number[i] << i;
+	}
+	return res;
+
+}
+
 int main(int argc, char* argv[])
 {
 
-	const int number_size = 4; //число двоичных разрядов в числе
+	const int number_size = 3; //число двоичных разрядов в числе
 	const int numbers_count = 10; //количество перемножаемых чисел
 	int proc_num, proc_rank;
 
@@ -100,7 +110,22 @@ int main(int argc, char* argv[])
 
 		cout << "sequential mul result: " << endl;
 		recv_numbers_lint_check[0].print();
+		cout << "res: " << endl;
+		long long res = to_dec(recv_numbers_lint_check[0]);
+		cout << res << endl;
 
+	}
+
+	MPI_Comm excl_last_comm;
+	MPI_Group world_group;
+	MPI_Comm_group(MPI_COMM_WORLD, &world_group);
+	MPI_Group group_without_last;
+	int rank_to_excl[1]{ proc_num - 1 };
+	MPI_Group_excl(world_group, 1, rank_to_excl, &group_without_last);
+	MPI_Comm_create(MPI_COMM_WORLD, group_without_last, &excl_last_comm);
+	int old_proc_num = proc_num;
+	if (proc_num % 2 != 0) {
+		--proc_num;
 	}
 
 	int recv_count;//число элементов, которое получит процесс
@@ -108,6 +133,10 @@ int main(int argc, char* argv[])
 	int* displs = nullptr;//смещения 
 	if (numbers_count >= proc_num) { //если чисел больше, чем процессов
 
+		longint recv_numbers_lint[numbers_count];
+		
+		if (!((proc_num + 1) % 2 != 0 && proc_rank == proc_num)) {
+		
 		int default_chunk_size = numbers_count / proc_num;//количество полиномов, которое получает каждый процесс
 														  //число оставшихся полиномов, кот нужно будет докинуть последним процессам
 		int nmbr_of_remaining_plnms = numbers_count % proc_num;
@@ -146,8 +175,6 @@ int main(int argc, char* argv[])
 			recv_count = default_chunk_size + 1;
 		}
 
-
-
 		char recv_numbers[numbers_count][number_size];
 
 		//int MPI_Scatterv(const void* sendbuf, const int sendcounts[],
@@ -156,36 +183,17 @@ int main(int argc, char* argv[])
 
 
 		//TODO делать этот этап, если число полиномов >=...
-		MPI_Scatterv(&(numbers[0][0]), send_counts, displs, MPI_CHAR, &(recv_numbers[0][0]), recv_count*number_size, MPI_CHAR, 0,
-			MPI_COMM_WORLD);
-
-		//каждый получил по числам
-
-		/*if (proc_rank == 1) {
-
-		for (int i = 0; i < recv_count; ++i) {
-		for (int j = 0; j < number_size; ++j) {
-		cout << recv_numbers[i][j] << " ";
+		if (old_proc_num % 2 != 0 && proc_rank != old_proc_num - 1) {
+			MPI_Scatterv(&(numbers[0][0]), send_counts, displs, MPI_CHAR, &(recv_numbers[0][0]), recv_count* number_size, MPI_CHAR, 0,
+				excl_last_comm);
 		}
-		cout << endl;
+		else {
+			MPI_Scatterv(&(numbers[0][0]), send_counts, displs, MPI_CHAR, &(recv_numbers[0][0]), recv_count* number_size, MPI_CHAR, 0,
+				MPI_COMM_WORLD);
 		}
-
-		}*/
 
 		//переводим все в long int
-		longint recv_numbers_lint[numbers_count];
 		arr_char_to_arr_longint(proc_rank, recv_count, recv_numbers, recv_numbers_lint);
-
-		/* if (proc_rank == 1) {
-
-		for (int i = 0; i < recv_count; ++i) {
-		for (int j = 0; j < number_size; ++j) {
-		cout << recv_numbers_lint[i].number[j] << " ";
-		}
-		cout << endl;
-		}
-
-		} */
 
 		//Теперь каждый процесс сначала перемножает свои числа
 		for (int i = 1; i < recv_count; ++i) {
@@ -193,139 +201,160 @@ int main(int argc, char* argv[])
 			recv_numbers_lint[0] = ss_mult(recv_numbers_lint[0], recv_numbers_lint[i]);
 		}
 
+	}
+
 		//теперь создаем сетку
-		//TODO для случая если, число процессов нечетно
 		int columns_num = proc_num / 2;
 		int rows_num = 2;
 		int dims[2]{ rows_num, columns_num };
 		int periods[2]{ 1, 1 };//на всякий случай 1 1
 		int reorder = 0; //пусть ранги процессов останутся прежними
 		MPI_Comm cart_comm;
-		MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &cart_comm);
-		int cart_rank = -1;
-		MPI_Comm_rank(cart_comm, &cart_rank);
-		//if (proc_rank == 7) print_arr(coords, 2);
 
-		//Создаем подсетку столбца
-		MPI_Comm column_comm;
-		int subdims[2]{ 1, 0 };
-		MPI_Cart_sub(cart_comm, subdims, &column_comm);
-		int row_num_rank = -1; //ранг, связанный с номером строки
-		MPI_Comm_rank(column_comm, &row_num_rank);
-
-		//на всякий случай, проверить координаты ранга
-
-		vector<char> result1 = recv_numbers_lint[0].number;
-		unsigned int result1_size = -1;
-		if (row_num_rank == 1) { //процессы из нижней строки отправляют числа процессам из верхней
-								 //сначала отправляем размер вектора, чтобы его знал получющий процесс
-			result1_size = result1.size();
-
-			MPI_Send(&result1_size, 1, MPI_UNSIGNED, 0, 0, column_comm);
-			MPI_Send(&result1[0], result1_size, MPI_CHAR, 0, 1, column_comm);
-
-
+		if (proc_num % 2 != 0) { //если число процессов нечетное
+			if (proc_rank != proc_num - 1) {
+				MPI_Cart_create(excl_last_comm, 2, dims, periods, reorder, &cart_comm);
+			}
+			--proc_num;
 		}
-		else if (row_num_rank == 0) {
-
-			unsigned int result2_size = -1;
-			vector<char> result2; //сюда будет записан вектор, полученный от нижнего процесса
-			MPI_Status status;
-			MPI_Recv(&result2_size, 1, MPI_UNSIGNED, 1, 0, column_comm, &status);
-			result2.resize(result2_size);
-			MPI_Recv(&result2[0], result2_size, MPI_UNSIGNED, 1, 1, column_comm, &status);
-
-			//перемножаем оба числа
-			longint result1_lint(result1);
-			longint result2_lint(result2);
-			result1_lint = ss_mult(result1_lint, result2_lint);
-			result1 = result1_lint.number;
-			result1_size = result1.size();
-
+		else {
+			MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &cart_comm);
 		}
 
-		//выделяем коммуникатор для первой строки
-		//Создаем группу для первой строки
-		int *coords = new int[2];
-		MPI_Cart_coords(cart_comm, cart_rank, 2, coords);
+		if (! ((proc_num + 1) % 2 != 0 && proc_rank == proc_num)) {
+			int cart_rank = -1;
+			MPI_Comm_rank(cart_comm, &cart_rank);
 
-		MPI_Comm row_comm;
-		int subdims2[2]{ 0, 1 };
-		MPI_Cart_sub(cart_comm, subdims2, &row_comm);
-		int column_num_rank = -1; //ранг, связанный с номером столбца
-		MPI_Comm_rank(row_comm, &column_num_rank);
 
-		if (coords[0] == 0) {
 
-			//узнаем от каждого процесса размер передаваемого вектора
-			const int col_num = columns_num;
-			unsigned int recv_counts[numbers_count]; //участвующих процессов все равно не всегда больше, чем чисел
-			MPI_Gather(&result1_size, 1, MPI_UNSIGNED, &recv_counts[0], 1, MPI_UNSIGNED, 0, row_comm);
+			//if (proc_rank == 7) print_arr(coords, 2);
 
-			//рассчитываем final_recv_counts и final_displs для передачи в mpi_gatherv
-			int max_vec_size = 0;
-			int *final_recv_counts = new int[proc_num / 2];
-			int *final_displs = new int[proc_num / 2];
-			if (column_num_rank == 0) {
-				int num_of_prev_elmnts = 0;
-				//cout << "recv_counts to 0 process" << endl;
-				for (int i = 0; i < proc_num / 2; ++i) {
-					if (recv_counts[i] > max_vec_size) max_vec_size = recv_counts[i];
-					final_recv_counts[i] = recv_counts[i];
-					final_displs[i] = num_of_prev_elmnts;
-					num_of_prev_elmnts += final_recv_counts[i];
-					//cout << recv_counts[i] << " ";
-				}
+			//Создаем подсетку столбца
+			MPI_Comm column_comm;
+			int subdims[2]{ 1, 0 };
+			MPI_Cart_sub(cart_comm, subdims, &column_comm);
+			int row_num_rank = -1; //ранг, связанный с номером строки
+			MPI_Comm_rank(column_comm, &row_num_rank);
+
+			//на всякий случай, проверить координаты ранга
+
+			vector<char> result1 = recv_numbers_lint[0].number;
+			unsigned int result1_size = -1;
+			if (row_num_rank == 1) { //процессы из нижней строки отправляют числа процессам из верхней
+									 //сначала отправляем размер вектора, чтобы его знал получющий процесс
+				result1_size = result1.size();
+
+				MPI_Send(&result1_size, 1, MPI_UNSIGNED, 0, 0, column_comm);
+				MPI_Send(&result1[0], result1_size, MPI_CHAR, 0, 1, column_comm);
+
+
+			}
+			else if (row_num_rank == 0) {
+
+				unsigned int result2_size = -1;
+				vector<char> result2; //сюда будет записан вектор, полученный от нижнего процесса
+				MPI_Status status;
+				MPI_Recv(&result2_size, 1, MPI_UNSIGNED, 1, 0, column_comm, &status);
+				result2.resize(result2_size);
+				MPI_Recv(&result2[0], result2_size, MPI_UNSIGNED, 1, 1, column_comm, &status);
+
+				//перемножаем оба числа
+				longint result1_lint(result1);
+				longint result2_lint(result2);
+				result1_lint = ss_mult(result1_lint, result2_lint);
+				result1 = result1_lint.number;
+				result1_size = result1.size();
 
 			}
 
-			//отправляем все результаты 0 процессу
-			//char **final_recv = allocate_2d_array(proc_num / 2, max_vec_size);
-			char *final_recv = new char[proc_num / 2 * max_vec_size];
-			MPI_Gatherv(&result1[0], result1_size, MPI_CHAR, final_recv, final_recv_counts, final_displs, MPI_CHAR, 0, row_comm);
+			//выделяем коммуникатор для первой строки
+			//Создаем группу для первой строки
+			int* coords = new int[2];
+			MPI_Cart_coords(cart_comm, cart_rank, 2, coords);
 
-			
-			if (column_num_rank == 0) {
+			MPI_Comm row_comm;
+			int subdims2[2]{ 0, 1 };
+			MPI_Cart_sub(cart_comm, subdims2, &row_comm);
+			int column_num_rank = -1; //ранг, связанный с номером столбца
+			MPI_Comm_rank(row_comm, &column_num_rank);
 
-				cout << "max_vec_size: " << max_vec_size << endl;
-				cout << "proc_num " << proc_num << endl;
-				for (int i = 0; i < proc_num / 2; ++i) {
-				cout << recv_counts[i] << " ";
-				}
+			if (coords[0] == 0) {
 
-				longint *final_arr = new longint[proc_num / 2];
-				
-				//cout << "j" << endl;
-				int num_of_prev_elements = 0;
-				for (int i = 0; i < proc_num / 2; ++i) {
-					vector<char> v;
-					for (int j = 0; j < final_recv_counts[i]; ++j) {
-						//cout << final_recv[num_of_prev_elements + j] + 0;
-						v.push_back(final_recv[num_of_prev_elements + j]);
+				//узнаем от каждого процесса размер передаваемого вектора
+				const int col_num = columns_num;
+				unsigned int recv_counts[numbers_count]; //участвующих процессов все равно не всегда больше, чем чисел
+				MPI_Gather(&result1_size, 1, MPI_UNSIGNED, &recv_counts[0], 1, MPI_UNSIGNED, 0, row_comm);
+
+				//рассчитываем final_recv_counts и final_displs для передачи в mpi_gatherv
+				int max_vec_size = 0;
+				int* final_recv_counts = new int[proc_num / 2];
+				int* final_displs = new int[proc_num / 2];
+				if (column_num_rank == 0) {
+					int num_of_prev_elmnts = 0;
+					//cout << "recv_counts to 0 process" << endl;
+					for (int i = 0; i < proc_num / 2; ++i) {
+						if (recv_counts[i] > max_vec_size) max_vec_size = recv_counts[i];
+						final_recv_counts[i] = recv_counts[i];
+						final_displs[i] = num_of_prev_elmnts;
+						num_of_prev_elmnts += final_recv_counts[i];
+						//cout << recv_counts[i] << " ";
 					}
-					num_of_prev_elements += final_recv_counts[i];
-					//cout << endl;
-					longint number(v);
-					final_arr[i] = number;
-				}
-				
-				for (int i = 1; i < proc_num / 2; ++i) {
-					final_arr[0] = ss_mult(final_arr[0], final_arr[i]);
+
 				}
 
-				vector<char> final_res = final_arr[0].number;
-				cout << endl;
-				cout << "result size: " << final_res.size() << endl;
+				//отправляем все результаты 0 процессу
+				//char **final_recv = allocate_2d_array(proc_num / 2, max_vec_size);
+				char* final_recv = new char[proc_num / 2 * max_vec_size];
+				MPI_Gatherv(&result1[0], result1_size, MPI_CHAR, final_recv, final_recv_counts, final_displs, MPI_CHAR, 0, row_comm);
 
-				cout << "parallel mul result: " << endl;
-				final_arr[0].print();
 
-				
+				if (column_num_rank == 0) {
+
+					cout << "max_vec_size: " << max_vec_size << endl;
+					cout << "proc_num " << proc_num << endl;
+					for (int i = 0; i < proc_num / 2; ++i) {
+						cout << recv_counts[i] << " ";
+					}
+
+					longint* final_arr = new longint[proc_num / 2];
+
+					//cout << "j" << endl;
+					int num_of_prev_elements = 0;
+					for (int i = 0; i < proc_num / 2; ++i) {
+						vector<char> v;
+						for (int j = 0; j < final_recv_counts[i]; ++j) {
+							//cout << final_recv[num_of_prev_elements + j] + 0;
+							v.push_back(final_recv[num_of_prev_elements + j]);
+						}
+						num_of_prev_elements += final_recv_counts[i];
+						//cout << endl;
+						longint number(v);
+						final_arr[i] = number;
+					}
+
+					for (int i = 1; i < proc_num / 2; ++i) {
+						final_arr[0] = ss_mult(final_arr[0], final_arr[i]);
+					}
+
+					vector<char> final_res = final_arr[0].number;
+					cout << endl;
+					cout << "result size: " << final_res.size() << endl;
+
+					cout << "parallel mul result: " << endl;
+					final_arr[0].print();
+					cout << "res: " << endl;
+					long long res = to_dec(final_arr[0]);
+					cout << res << endl;
+
+
+				}
+
+
+
 			}
-
-			
-
 		}
+
+		
 		
 
 	}
